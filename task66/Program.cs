9b8f6499afdfcae120f4895ed060b80493abd12a
sergey <ootieroo@gmail.com>
2022-07-28 14:36:05 +0300

    homework 09

---
+++ task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// задание 66
+
+
+int m = InputInt("Введите первое число M:");
+int n = InputInt("Введите второе число N:");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
